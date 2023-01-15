5064299865150ca1ee20f7feb1e22478d669ad3d
shaphat-pixel <khalshaphat@gmail.com>
2023-01-15 17:48:34 +0000

    psycopg2

--- mis/settings.py
+++ mis/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-a9+(z!x#_hm&e&0@(z%#^@rly-lxppvp*@bmjvf&9ld29(wjkv
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['http://*']
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://']
 
 # Application definition
 
