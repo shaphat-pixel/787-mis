0776c4776ee74721c7612d32d60a459f6555730c
shaphat-pixel <khalshaphat@gmail.com>
2023-01-20 00:53:56 +0000

    tried to fix some migration issues

--- mis/settings.py
+++ mis/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-a9+(z!x#_hm&e&0@(z%#^@rly-lxppvp*@bmjvf&9ld29(wjkv
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://backup-gamkrib.herokuapp.com/']
+CSRF_TRUSTED_ORIGINS = ['https://www.mis.tdcgh.com/']
 
 # Application definition
 
